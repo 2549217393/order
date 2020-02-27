bc5e23d52dc586b484ad425c0a46bcc018bc8e52
2549217393 <2549217393@qq.com>
2020-02-27 10:13:31 +0800

    拦截器过滤器监听器

---
+++ src/main/java/com/bdqn/order/util/constant/SysUtil.java
@@ -0,0 +1,9 @@
+package com.bdqn.order.util.constant;
+
+public class SysUtil {
+    /**
+     * 在线人数
+     */
+    public static Integer ONLINE_COUNT = 0;
+}
+
---
+++ src/main/java/com/bdqn/order/util/filter/HttpSession1Listener.java
@@ -0,0 +1,28 @@
+package com.bdqn.order.util.filter;
+
+import com.bdqn.order.util.constant.SysUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@WebListener
+public class HttpSession1Listener implements HttpSessionListener {
+    Logger logger = LoggerFactory.getLogger(HttpSession1Listener.class);
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        logger.info("session增加");
+        SysUtil.ONLINE_COUNT += 1;
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        logger.info("session销毁");
+        SysUtil.ONLINE_COUNT -= 1;
+    }
+}
